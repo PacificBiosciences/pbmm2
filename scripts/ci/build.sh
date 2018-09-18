a140d88e11b19c6656e1866b6fbe7f0a98c612a3
Armin Toepfer <atoepfer@pacificbiosciences.com>
2018-09-18 09:17:57 +0200

    Update build plan

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,15 +5,16 @@ set -euo pipefail
 echo "# LOAD MODULES"
 source /mnt/software/Modules/current/init/bash
 
-module load gcc git ccache boost ninja meson seqan gtest pbcopper zlib htslib parasail cram bedtools datamash pbbam samtools
+module load gcc git ccache boost htslib/1.8 ninja meson gtest zlib cram bedtools datamash
 meson build-test .
 ninja -C build-test
 
 if [[ "${bamboo_planRepository_branchName}" == "develop" ]]; then
     echo "Copy binary to module"
     module purge
-    module load gcc git ccache boost ninja meson seqan gtest htslib/static zlib/static
-    LDFLAGS='-lrt' CXXFLAGS="-static-libgcc -static-libstdc++" meson --strip build-static . --prefix / -Dtests=false --default-library=static
+    module load htslib/1.8-static zlib/static gcc git ccache boost ninja meson gtest
+    # LDFLAGS='-lrt' CXXFLAGS="-static-libgcc -static-libstdc++" meson --strip build-static . --prefix / -Dtests=false --default-library=static
+    meson --strip build-static . --prefix / -Dtests=false
     ninja -C build-static
     DESTDIR="/mnt/software/p/pbmm2/current" ninja -C build-static -v install
 fi
