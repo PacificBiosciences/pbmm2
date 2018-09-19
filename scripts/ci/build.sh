28e87102a907fded131e267b68ab96d52f282c31
Armin Toepfer <atoepfer@pacificbiosciences.com>
2018-09-19 15:45:32 +0200

    Do not copy to module

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,16 +5,7 @@ set -euo pipefail
 echo "# LOAD MODULES"
 source /mnt/software/Modules/current/init/bash
 
-module load gcc git ccache boost htslib/1.8 ninja meson gtest zlib cram bedtools datamash
+module load gcc git ccache boost htslib ninja meson gtest zlib cram bedtools datamash samtools
 meson build-test .
 ninja -C build-test
-
-if [[ "${bamboo_planRepository_branchName}" == "develop" ]]; then
-    echo "Copy binary to module"
-    module purge
-    module load htslib/1.8-static zlib/static gcc git ccache boost ninja meson gtest
-    # LDFLAGS='-lrt' CXXFLAGS="-static-libgcc -static-libstdc++" meson --strip build-static . --prefix / -Dtests=false --default-library=static
-    meson --strip build-static . --prefix / -Dtests=false
-    ninja -C build-static
-    DESTDIR="/mnt/software/p/pbmm2/current" ninja -C build-static -v install
-fi
+ninja -C build-test test
