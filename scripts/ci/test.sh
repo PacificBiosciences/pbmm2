925bce1a1c490b67221b7ecba03bf4d49fb3d60b
David Seifert <dseifert@pacificbiosciences.com>
2018-10-26 14:46:17 -0700

    Use cd in subshell for directory traversal

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -11,10 +11,9 @@ ninja -C "${CURRENT_BUILD_DIR:-build}" -v test
 # COVERAGE #
 ############
 
-pushd "${CURRENT_BUILD_DIR:-build}" >/dev/null
-  find . -type f -iname '*.o' | xargs gcov -acbrfu {} \; >/dev/null && \
-    mkdir coverage && pushd coverage && mv ../*.gcov . && \
-    sed -i -e 's@Source:@Source:../@' *.gcov && \
-    sed -i -e 's@Graph:@Graph:../@' *.gcov && \
-    sed -i -e 's@Data:@Data:../@' *.gcov
-popd >/dev/null
+( cd "${CURRENT_BUILD_DIR:-build}" &&
+  find . -type f -iname '*.o' | xargs gcov -acbrfu {} \; >/dev/null &&
+    mkdir coverage && cd coverage && mv ../*.gcov . &&
+    sed -i -e 's@Source:@Source:../@' *.gcov &&
+    sed -i -e 's@Graph:@Graph:../@' *.gcov &&
+    sed -i -e 's@Data:@Data:../@' *.gcov )
