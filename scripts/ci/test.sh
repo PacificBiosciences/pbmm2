74320574f864e2a8b707bffc9cf59902875f35f6
David Seifert <dseifert@pacificbiosciences.com>
2018-10-26 14:30:31 -0700

    Add pushd/popd for directory traversal

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -11,9 +11,10 @@ ninja -C "${CURRENT_BUILD_DIR:-build}" -v test
 # COVERAGE #
 ############
 
-cd "${CURRENT_BUILD_DIR:-build}"
-find . -type f -iname '*.o' | xargs gcov -acbrfu {} \; >/dev/null && \
-  mkdir coverage && pushd coverage && mv ../*.gcov . && \
-  sed -i -e 's@Source:@Source:../@' *.gcov && \
-  sed -i -e 's@Graph:@Graph:../@' *.gcov && \
-  sed -i -e 's@Data:@Data:../@' *.gcov
+pushd "${CURRENT_BUILD_DIR:-build}" >/dev/null
+  find . -type f -iname '*.o' | xargs gcov -acbrfu {} \; >/dev/null && \
+    mkdir coverage && pushd coverage && mv ../*.gcov . && \
+    sed -i -e 's@Source:@Source:../@' *.gcov && \
+    sed -i -e 's@Graph:@Graph:../@' *.gcov && \
+    sed -i -e 's@Data:@Data:../@' *.gcov
+popd >/dev/null
