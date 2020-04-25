2b87909bb80738b101b7ffac80449a9ae0146745
David Seifert <dseifert@pacificbiosciences.com>
2020-04-25 21:33:14 +0200

    Improve instrumentation

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -10,13 +10,16 @@ set -vex
 # stuff from the internet or using subprojects.
 meson \
   --buildtype "${BUILDTYPE:-release}" \
-  --default-library shared \
+  --default-library "${LIBRARYTYPE:-shared}" \
   --libdir lib \
   --unity "${ENABLED_UNITY_BUILD:-off}" \
-  --wrap-mode "${WRAP_MODE:-nofallback}" \
+  --wrap-mode "${ENABLED_WRAP_MODE:-nofallback}" \
   --prefix "${PREFIX_ARG:-/usr/local}" \
   -Db_coverage="${ENABLED_COVERAGE:-false}" \
+  -Db_lto="${ENABLED_LTO:-false}" \
   -Db_sanitize="${ENABLED_SANITIZERS:-none}" \
+  -Db_lundef="${ENABLED_LUNDEF:-true}" \
+  -Dcpp_debugstl="${ENABLED_DEBUGSTL:-false}" \
   -Dtests="${ENABLED_TESTS:-false}" \
   "${CURRENT_BUILD_DIR:-build}" .
 
