a8dcb5a7e53c33ebfa416e1af395966074915160
Derek Barnett <dbarnett@pacificbiosciences.com>
2020-05-15 16:14:59 -0500

    Provide library version/dependency info.

---
+++ include/pbmm2/LibraryInfo.h
@@ -0,0 +1,27 @@
+// Author: Derek Barnett
+
+#pragma once
+
+#include <pbcopper/library/Bundle.h>
+#include <pbcopper/library/Info.h>
+
+namespace PacBio {
+namespace Pbmm2 {
+
+///
+/// \return pbmm2 library info (e.g. name, version)
+///
+Library::Info LibraryInfo();
+
+///
+/// \returns bundle of pbmm2 library info, plus its dependencies
+///
+Library::Bundle LibraryBundle();
+
+///
+/// \return minimap2 library info (pbmm2 dependency)
+///
+Library::Info Minimap2LibraryInfo();
+
+}  // namespace Pbmm2
+}  // namespace PacBio
